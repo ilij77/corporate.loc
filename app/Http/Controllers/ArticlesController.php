36b1f4129ee4e732771a2f783fd6ac706e6cd7c3
Iliya <ilij@ya.ru>
2018-12-06 03:46:31 +0300

    Урок 20.

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Corp\Http\Controllers;
+
+use Corp\Category;
+use Corp\Repositories\ArticlesRepository;
+use Corp\Repositories\CommentsRepository;
+use Corp\Repositories\PortfoliosRepository;
+use Illuminate\Http\Request;
+
+class ArticlesController extends SiteController
+{
+    public function __construct(PortfoliosRepository$p_rep,ArticlesRepository $a_rep,CommentsRepository $c_rep)
+    {
+        parent::__construct(new\Corp\Repositories\MenusRepository(new \Corp\Menu));
+        $this->template=env('THEME').'.articles';
+        $this->bar='right';
+        $this->p_rep=$p_rep;
+        $this->a_rep=$a_rep;
+        $this->c_rep=$c_rep;
+    }
+
+    public function index($cat_alias=false)
+    {
+        $articles=$this->getArticles($cat_alias);
+
+  //dd($articles);
+
+        $content=view(env('THEME').'.articles_content')->with('articles',$articles)->render();
+        $this->vars=array_add($this->vars,'content',$content);
+
+        $comments=$this->getComments(config('settings.resent_comments'));
+        $portfolios=$this->getPortfolios(config('settings.resent_portfolios'));
+
+       // dd($comments);
+       //dd($portfolios);
+
+        $this->contentRightBar=view(env('THEME').'.articlesBar')->with(['comments'=>$comments,'portfolios'=>$portfolios]);
+
+        return $this->renderOutput();
+    }
+
+    public function getComments($take){
+
+        $comments=$this->c_rep->get(['text','name','email','site','article_id','user_id'],$take);
+        if ($comments) {
+            $comments->load('article', 'user');
+        }
+
+        return $comments;
+    }
+
+    public function getportfolios($take){
+
+        $portfolios=$this->p_rep->get(['title','text','alias','customer','img','filter_alias'],$take);
+        return $portfolios;
+    }
+
+
+
+    public function getArticles($alias=false){
+
+        $where=false;
+
+        if ($alias){
+
+            //WHERE 'alias'=$alias
+            $id=Category::select('id')->where('alias',$alias)->first();
+            //dd($id);
+            $where=['category_id',$id];
+            //dd($where);
+        }
+     $articles=$this->a_rep->get(['id','title','alias','created_at','img','desc','user_id','category_id'],false,true,$where);
+
+    if ($articles){
+        $articles->load('user','category','comment');
+    }
+    return$articles;
+    }
+
+ public function show($alias=false){
+
+     $this->template=env('THEME').'.articles';
+
+        $article=$this->a_rep->one($alias,['comments'=>true]);
+        if ($article){
+            $article->img=json_decode($article->img);
+        }
+       // dd($article->comment->groupBy('parent_id'));
+
+     //dd($article->user->name);
+     //dd($article->title);
+        $content=view(env('THEME').'.article_content')->with('article',$article)->render();
+        $this->vars=array_add($this->vars,'content',$content);
+
+     $comments=$this->getComments(config('settings.resent_comments'));
+     $portfolios=$this->getPortfolios(config('settings.resent_portfolios'));
+     $this->contentRightBar=view(env('THEME').'.articlesBar')->with(['comments'=>$comments,'portfolios'=>$portfolios]);
+
+        return$this->renderOutput();
+ }
+
+}
---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Corp\Http\Controllers;
+
+use Corp\Article;
+use Corp\Comment;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
+
+class CommentController extends SiteController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+
+        $data=$request->except('_token','comment_post_ID','comment_parent');
+        //print_r($data);
+        $data['article_id']=$request->input('comment_post_ID');
+        $data['parent_id']=$request->input('comment_parent');
+        $validator=Validator::make($data,[
+
+            'article_id'=>'integer | required',
+            'parent_id'=>'integer | required',
+            'text'=>'string | required',
+
+        ]);
+
+        $validator->sometimes(['name','email'],'required | max:255',function ($input){
+            return !Auth::check();
+        });
+
+        if ($validator->fails()){
+            return \Response::json(['error'=>$validator->errors()->all()]);
+        }
+
+        $user=Auth::user();
+
+        $comment=new Comment($data);
+        if ($user){
+            $comment->user()==$user->id;
+        }
+        $post=Article::find($data['article_id']);
+        $post->comment()->save($comment);
+
+exit();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
