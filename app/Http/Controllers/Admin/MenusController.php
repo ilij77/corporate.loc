6f0fc80473be953171d0e32bf4ef18e51951e4b9
Iliya <ilij@ya.ru>
2018-12-16 02:42:07 +0300

    Урок 44

--- app/Http/Controllers/Admin/MenusController.php
+++ app/Http/Controllers/Admin/MenusController.php
@@ -19,13 +19,13 @@ class MenusController extends AdminController
 
     protected $m_rep;
 
-    public function __construct(MenusRepository$m_rep,ArticlesRepository $a_rep,PortfoliosRepository $p_rep)
+    public function __construct(MenusRepository $m_rep, ArticlesRepository $a_rep, PortfoliosRepository $p_rep)
     {
         parent::__construct();
-        $this->m_rep=$m_rep;
-        $this->a_rep=$a_rep;
-        $this->p_rep=$p_rep;
-        $this->template=env('THEME').'.admin.menus';
+        $this->m_rep = $m_rep;
+        $this->a_rep = $a_rep;
+        $this->p_rep = $p_rep;
+        $this->template = env('THEME') . '.admin.menus';
 
 
     }
@@ -38,15 +38,15 @@ class MenusController extends AdminController
      */
     public function index()
     {
-        $this->user=Auth::user();
-        if ( Gate::denies('VIEW_ADMIN_MENU')){
+        $this->user = Auth::user();
+        if (Gate::denies('VIEW_ADMIN_MENU')) {
             abort(403);
         };
 
-      //dd($this->user);
-        $menu=$this->getMenus();
+        //dd($this->user);
+        $menu = $this->getMenus();
         //dd($menu);
-        $this->content=view(env('THEME').'.admin.menus_content')->with('menus',$menu)->render();
+        $this->content = view(env('THEME') . '.admin.menus_content')->with('menus', $menu)->render();
         return $this->renderOutput();
 
 
@@ -55,27 +55,25 @@ class MenusController extends AdminController
 
     public function getMenus()
     {
-        $menu=$this->m_rep->get();
-        if ($menu->isEmpty()){
+        $menu = $this->m_rep->get();
+        if ($menu->isEmpty()) {
             return false;
 
         }
 
-        return \Menu::make('forMenuPart',function ($m) use($menu){
+        return \Menu::make('forMenuPart', function ($m) use ($menu) {
             foreach ($menu as $item) {
-                if ($item->parent==0){
-                    $m->add($item->title,$item->path)->id($item->id);
-                }
-                else{
-                    if ($m->find($item->parent)){
-                        $m->find($item->parent)->add($item->title,$item->path)->id($item->id);
+                if ($item->parent == 0) {
+                    $m->add($item->title, $item->path)->id($item->id);
+                } else {
+                    if ($m->find($item->parent)) {
+                        $m->find($item->parent)->add($item->title, $item->path)->id($item->id);
                     }
                 }
             }
         });
 
     }
-
 
 
     /**
@@ -85,59 +83,57 @@ class MenusController extends AdminController
      */
     public function create()
     {
-      $this->title='Новый пункт меню';
-      $tmp=$this->getMenus()->roots();
+        $this->title = 'Новый пункт меню';
+        $tmp = $this->getMenus()->roots();
 //dd($tmp);
-      $menus=$tmp->reduce(function ($returMenus,$menu){
-          $returMenus[$menu->id]=$menu->title;
-          return $returMenus;
+        $menus = $tmp->reduce(function ($returMenus, $menu) {
+            $returMenus[$menu->id] = $menu->title;
+            return $returMenus;
 
-      },['0'=>'Родительский пункт меню']);
+        }, ['0' => 'Родительский пункт меню']);
 
         //dd($menus);
 
 
-        $categories=Category::select(['title','alias','parent_id','id'])->get();
-   //dd($categories);
+        $categories = Category::select(['title', 'alias', 'parent_id', 'id'])->get();
+        //dd($categories);
 
-        $list=array();
-        $list=array_add($list,'0','Не используется');
-        $list=array_add($list,'parent','Раздел блог');
-        foreach ($categories as $category){
-            if ($category->parent_id ==0){
+        $list = array();
+        $list = array_add($list, '0', 'Не используется');
+        $list = array_add($list, 'parent', 'Раздел блог');
+        foreach ($categories as $category) {
+            if ($category->parent_id == 0) {
                 $list[$category->title] = array();
-            }
-            else{
-                $list[$categories->where('id',$category->parent_id)->first()->title] [$category->alias] = $category->title;
+            } else {
+                $list[$categories->where('id', $category->parent_id)->first()->title] [$category->alias] = $category->title;
             }
         }
 
-       // dd($list);
-        $aricles=$this->a_rep->get(['id','title','alias']);
+        // dd($list);
+        $aricles = $this->a_rep->get(['id', 'title', 'alias']);
         //dd($aricles);
-        $articles=$aricles->reduce(function ($returnArticles,$articles){
-                $returnArticles[$articles->id]=$articles->title;
-                return $returnArticles;
+        $articles = $aricles->reduce(function ($returnArticles, $articles) {
+            $returnArticles[$articles->id] = $articles->title;
+            return $returnArticles;
 
-            },[]);
+        }, []);
         //dd($articles);
 
-        $filters = \Corp\Filter::select('id','title','alias')->get()->reduce(function ($returnFilters, $filter) {
+        $filters = \Corp\Filter::select('id', 'title', 'alias')->get()->reduce(function ($returnFilters, $filter) {
             $returnFilters[$filter->alias] = $filter->title;
             return $returnFilters;
         }, ['parent' => 'Раздел портфолио']);
-            //dd($filters);
+        //dd($filters);
 
 
-        $portfolios = $this->p_rep->get(['id','alias','title'])->reduce(function ($returnPortfolios, $portfolio) {
+        $portfolios = $this->p_rep->get(['id', 'alias', 'title'])->reduce(function ($returnPortfolios, $portfolio) {
             $returnPortfolios[$portfolio->alias] = $portfolio->title;
             return $returnPortfolios;
         }, []);
 
         //dd($portfolios);
 
-        $this->content = view(env('THEME').'.admin.menus_create_content')->with(['menus'=>$menus,'categories'=>$list,'articles'=>$articles,'filters' => $filters,'portfolios' => $portfolios])->render();
-
+        $this->content = view(env('THEME') . '.admin.menus_create_content')->with(['menus' => $menus, 'categories' => $list, 'articles' => $articles, 'filters' => $filters, 'portfolios' => $portfolios])->render();
 
 
         return $this->renderOutput();
@@ -149,14 +145,14 @@ class MenusController extends AdminController
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(MenusRequest $request)
     {
         //dd($request);
-        $result=$this->m_rep->addMenu($request);
-        if (is_array($result) && !empty($result['error'])){
+        $result = $this->m_rep->addMenu($request);
+        if (is_array($result) && !empty($result['error'])) {
             return back()->with($result);
         }
         //dd($result);
@@ -166,7 +162,7 @@ class MenusController extends AdminController
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -177,103 +173,93 @@ class MenusController extends AdminController
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit(\Corp\Menu $menu)
     {
         //dd($menu);
-        $type=false;
-        $option=false;
+        $type = false;
+        $option = false;
 
 
+        $route = app('router')->getRoutes()->match(app('request')->create($menu->path));
 
-       $route=app('router')->getRoutes()->match(app('request')->create($menu->path));
-
-        $aliasRoute=$route->getName();
-        $parameters=$route->parameters();
+        $aliasRoute = $route->getName();
+        $parameters = $route->parameters();
         //dd($parameters);
 
         //dump($aliasRoute);
         //dump($parameters);
 
-        if ($aliasRoute=='articles.index' || $aliasRoute=='articlesCat' ){
-            $type='blogLink';
-            $option=isset($parameters['cat_alias']) ? $parameters['cat_alias'] : 'parent';
-        }
-
-        elseif ($aliasRoute=='articles.show' ){
-            $type='blogLink';
-            $option=isset($parameters['alias']) ? $parameters['alias'] : '';
-        }
-        elseif ($aliasRoute=='portfolios.index' ){
-            $type='portfolioLink';
-            $option='parent';
-        }
-        elseif ($aliasRoute=='portfolios.show' ){
-            $type='portfolioLink';
-            $option=isset($parameters['alias']) ? $parameters['alias'] : '';
-        }
-        else{
-            $type='customLink';
+        if ($aliasRoute == 'articles.index' || $aliasRoute == 'articlesCat') {
+            $type = 'blogLink';
+            $option = isset($parameters['cat_alias']) ? $parameters['cat_alias'] : 'parent';
+        } elseif ($aliasRoute == 'articles.show') {
+            $type = 'blogLink';
+            $option = isset($parameters['alias']) ? $parameters['alias'] : '';
+        } elseif ($aliasRoute == 'portfolios.index') {
+            $type = 'portfolioLink';
+            $option = 'parent';
+        } elseif ($aliasRoute == 'portfolios.show') {
+            $type = 'portfolioLink';
+            $option = isset($parameters['alias']) ? $parameters['alias'] : '';
+        } else {
+            $type = 'customLink';
         }
 
 
-
-
-        $this->title='Редактирование ссылки -'.$menu->title;
-        $tmp=$this->getMenus()->roots();
+        $this->title = 'Редактирование ссылки -' . $menu->title;
+        $tmp = $this->getMenus()->roots();
 //dd($tmp);
-        $menus=$tmp->reduce(function ($returMenus,$menu){
-            $returMenus[$menu->id]=$menu->title;
+        $menus = $tmp->reduce(function ($returMenus, $menu) {
+            $returMenus[$menu->id] = $menu->title;
             return $returMenus;
 
-        },['0'=>'Родительский пункт меню']);
+        }, ['0' => 'Родительский пункт меню']);
 
         //dd($menus);
 
 
-        $categories=Category::select(['title','alias','parent_id','id'])->get();
+        $categories = Category::select(['title', 'alias', 'parent_id', 'id'])->get();
         //dd($categories);
 
-        $list=array();
-        $list=array_add($list,'0','Не используется');
-        $list=array_add($list,'parent','Раздел блог');
-        foreach ($categories as $category){
-            if ($category->parent_id ==0){
+        $list = array();
+        $list = array_add($list, '0', 'Не используется');
+        $list = array_add($list, 'parent', 'Раздел блог');
+        foreach ($categories as $category) {
+            if ($category->parent_id == 0) {
                 $list[$category->title] = array();
-            }
-            else{
-                $list[$categories->where('id',$category->parent_id)->first()->title] [$category->alias] = $category->title;
+            } else {
+                $list[$categories->where('id', $category->parent_id)->first()->title] [$category->alias] = $category->title;
             }
         }
 
         // dd($list);
-        $aricles=$this->a_rep->get(['id','title','alias']);
+        $aricles = $this->a_rep->get(['id', 'title', 'alias']);
         //dd($aricles);
-        $articles=$aricles->reduce(function ($returnArticles,$articles){
-            $returnArticles[$articles->id]=$articles->title;
+        $articles = $aricles->reduce(function ($returnArticles, $articles) {
+            $returnArticles[$articles->id] = $articles->title;
             return $returnArticles;
 
-        },[]);
+        }, []);
         //dd($articles);
 
-        $filters = \Corp\Filter::select('id','title','alias')->get()->reduce(function ($returnFilters, $filter) {
+        $filters = \Corp\Filter::select('id', 'title', 'alias')->get()->reduce(function ($returnFilters, $filter) {
             $returnFilters[$filter->alias] = $filter->title;
             return $returnFilters;
         }, ['parent' => 'Раздел портфолио']);
         //dd($filters);
 
 
-        $portfolios = $this->p_rep->get(['id','alias','title'])->reduce(function ($returnPortfolios, $portfolio) {
+        $portfolios = $this->p_rep->get(['id', 'alias', 'title'])->reduce(function ($returnPortfolios, $portfolio) {
             $returnPortfolios[$portfolio->alias] = $portfolio->title;
             return $returnPortfolios;
         }, []);
 
         //dd($portfolios);
 
-        $this->content = view(env('THEME').'.admin.menus_create_content')->with(['menu'=>$menu,'type'=>$type,'option'=>$option,'menus'=>$menus,'categories'=>$list,'articles'=>$articles,'filters' => $filters,'portfolios' => $portfolios])->render();
-
+        $this->content = view(env('THEME') . '.admin.menus_create_content')->with(['menu' => $menu, 'type' => $type, 'option' => $option, 'menus' => $menus, 'categories' => $list, 'articles' => $articles, 'filters' => $filters, 'portfolios' => $portfolios])->render();
 
 
         return $this->renderOutput();
@@ -282,27 +268,40 @@ class MenusController extends AdminController
     }
 
 
-
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, \Corp\Menu $menu)
     {
-        //
+        //dd($request);
+        $result = $this->m_rep->updateMenu($request, $menu);
+        if (is_array($result) && !empty($result['error'])) {
+            return back()->with($result);
+        }
+        //dd($result);
+        return redirect('/admin')->with($result);
     }
+
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(\Corp\Menu $menu)
     {
-        //
+        //dd($request);
+        $result = $this->m_rep->deleteMenu($menu);
+        if (is_array($result) && !empty($result['error'])) {
+            return back()->with($result);
+        }
+        //dd($result);
+        return redirect('/admin')->with($result);
     }
 }
+
--- app/Menu.php
+++ app/Menu.php
@@ -9,4 +9,10 @@ class Menu extends Model
     protected $fillable = [
         'title', 'path','parent'
     ];
+    public function delete(array $options=[]){
+        self::where('parent',$this->id)->delete();
+
+
+        return parent::delete($options);
+    }
 }
